453ebd3bbca2edec8126c7cb244465016054cadd
amossg <133963003+amossg@users.noreply.github.com>
2023-05-27 20:50:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def mem_test2():
 api.add_resource(Main, "/api/somedata/<int:somedata_id>", "/mem", "/mem2")
 api.init_app(app)
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(debug=True, port=80)
 
 with open('/flask_sr/base/base.json', 'w') as outfile:
     json.dump(somedata, outfile)
