db371bdcae35606d9d435719fe474f7bd106c925
amossg <133963003+amossg@users.noreply.github.com>
2023-07-06 16:36:56 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,100 @@
+# create VPC
+resource "google_compute_network" "vpc" {
+  name                    = "vpc1"
+  auto_create_subnetworks = false
+}
+
+# Create Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "subnet1"
+  region        = "europe-central2"
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.0.0.0/24"
+
+resource "google_container_cluster" "primary" {
+  name                     = "my-gke-cluster"
+  location                 = "europe-central2-b"
+  network                  = google_compute_network.vpc.name
+  subnetwork               = google_compute_subnetwork.subnet.name
+  remove_default_node_pool = true                ## create the smallest possible default node pool and immediately delete it.
+  # networking_mode          = "VPC_NATIVE" 
+  initial_node_count       = 1
+
+  private_cluster_config {
+    enable_private_endpoint = true
+    enable_private_nodes   = true 
+    master_ipv4_cidr_block = "10.13.0.0/28"
+  }
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = "10.11.0.0/21"
+    services_ipv4_cidr_block = "10.12.0.0/21"
+  }
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = "10.0.0.7/32"
+      display_name = "net1"
+    }
+
+  }
+}
+
+# Create managed node pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = google_container_cluster.primary.name
+  location   = "europe-central2-b"
+  cluster    = google_container_cluster.primary.name
+  node_count = 3
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = "dev"
+    }
+
+    machine_type = "f1-micro"
+    preemptible  = true
+    #service_account = google_service_account.mysa.email
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
+
+
+## Create jump host . We will allow this jump host to access GKE cluster. the ip of this jump host is already authorized to allowin the GKE cluster
+
+resource "google_compute_address" "my_internal_ip_addr" {
+  project      = "celtic-beacon-387519"
+  address_type = "INTERNAL"
+  region       = "europe-central2"
+  subnetwork   = "subnet1"
+  name         = "my-ip"
+  address      = "10.0.0.7"
+  description  = "An internal IP address for my jump host"
+}
+
+resource "google_compute_instance" "default" {
+  project      = "tcb-project-371706"
+  zone         = "asia-south2-a"
+  name         = "jump-host"
+  machine_type = "f1-micro"
+
+ boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network    = "vpc1"
+    subnetwork = "subnet1" # Replace with a reference or self link to your subnet, in quotes
+    network_ip         = google_compute_address.my_internal_ip_addr.address
+  }
+
+}
+
+
