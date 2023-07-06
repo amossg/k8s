b8c00564de3e356b01f4d285833078a22d35c120
amossg <133963003+amossg@users.noreply.github.com>
2023-07-06 15:59:12 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "project_id" {
   description = "The project ID of your project"
-  default     = "celtic-beacon-387519"
+  default     = "Flask"
 }
 variable "cluster_name" {
   description = "The name for the GKE cluster"
