4ee49b3945cee186ba24227bcb4e895b9448caa5
amossg <133963003+amossg@users.noreply.github.com>
2023-07-07 14:43:32 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+# define the GCP authentication file
+variable "gcp_auth_file" {
+  type = string
+  description = "GCP authentication file"
+}
+# define GCP project name
+variable "app_project" {
+  type = string
+  description = "GCP project name"
+}
+# define GCP region
+variable "gcp_region_1" {
+  type = string
+  description = "GCP region"
+}
+# define GCP zone
+variable "gcp_zone_1" {
+  type = string
+  description = "GCP zone"
+}
