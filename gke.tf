4134c4d02e5180b43ced0cf930f705eee8595950
amossg <133963003+amossg@users.noreply.github.com>
2023-07-08 14:10:43 +0300

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -20,14 +20,20 @@ resource "google_container_cluster" "primary" {
 
 resource "google_container_node_pool" "primary_nodes" {
   name       = google_container_cluster.primary.name
-  location   = "europe-central2-b"
+  location   = var.region
   cluster    = google_container_cluster.primary.name
   node_count = var.gke_num_nodes
 
   node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
     disk_size_gb = 50
-  }
-
+    labels = {
+      env = var.project_id
+    }
+    
     # preemptible  = true
     machine_type = "n1-standard-1"
     tags         = ["gke-node", "${var.project_id}-gke"]
@@ -35,3 +41,4 @@ resource "google_container_node_pool" "primary_nodes" {
       disable-legacy-endpoints = "true"
     }
   }
+}
