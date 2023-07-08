c381241c33733a3db05ceebb4247fcbe6a66d24c
amossg <133963003+amossg@users.noreply.github.com>
2023-07-08 15:58:44 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -62,7 +62,6 @@ module "gke" {
   ip_range_pods          = var.ip_range_pods_name
   ip_range_services      = var.ip_range_services_name
   create_service_account = true
-}
 
   node_pools = [
     {
@@ -85,3 +84,4 @@ module "gke" {
       initial_node_count        = 2
     },
   ]
+}
