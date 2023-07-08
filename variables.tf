bc45afb541d18e3c9763e2ddff6d1cad961c0e87
amossg <133963003+amossg@users.noreply.github.com>
2023-07-08 16:42:49 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+
 variable "project_id" {
   description = "The project ID to host the cluster in"
 }
@@ -5,35 +6,29 @@ variable "project_id" {
 variable "cluster_name" {
   description = "The name for the GKE cluster"
   default     = "gke-on-vpc-cluster"
-  type = string
 }
 
 variable "region" {
   description = "The region to host the cluster in"
-  default     = "europe-central2"
-  type = string
+  default     = "us-central1"
 }
 
 variable "network" {
   description = "The VPC network created to host the cluster in"
   default     = "gke-network"
-  type = string
 }
 
 variable "subnetwork" {
   description = "The subnetwork created to host the cluster in"
-  default     = "gke-subnet" 
-  type = string
+  default     = "gke-subnet"
 }
 
 variable "ip_range_pods_name" {
   description = "The secondary ip range to use for pods"
   default     = "ip-range-pods"
-  type = string
 }
 
 variable "ip_range_services_name" {
   description = "The secondary ip range to use for services"
   default     = "ip-range-svc"
-  type = string
 }
